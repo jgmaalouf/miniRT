5f1ada902d0c268d53d13fcf72520059a3152dbc
amorvai <amorvai@student.42heilbronn.de>
2023-03-06 14:11:15 +0100

    added due to changes missing unistd.h include

--- include/minirt.h
+++ include/minirt.h
@@ -2,9 +2,10 @@
 # define MINIRT_H
 
 # include <fcntl.h>
+# include <unistd.h>
 # include <stdio.h>
-# include <stdbool.h>
 # include <stdlib.h>
+# include <stdbool.h>
 # include "lib.h"
 # include "elem_count.h"
 # include "vector.h"
