f846df061d8d72f3da10c4c3b85879f870983bb4
amorvai <amorvai@student.42heilbronn.de>
2023-03-09 12:26:52 +0100

    first simple functions

---
+++ include/ray.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ray.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amorvai <amorvai@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/09 11:32:04 by amorvai           #+#    #+#             */
+/*   Updated: 2023/03/09 11:39:29 by amorvai          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RAY_H
+# define RAY_H
+
+#include "vector.h"
+
+typedef struct s_ray
+{
+	t_point3	orig;
+	t_vec3		dir;
+}				t_ray;
+
+t_ray		ray_constr(const t_point3 origin, const t_vec3 direction);
+
+t_point3	ray_origin(const t_ray ray);
+t_vec3		ray_direction(const t_ray ray);
+
+t_point3	ray_at(const t_ray ray, const double t);
+
+#endif
