e26912bdb899d66f1e1368b8d4f886f067443cad
Anna <annama377@gmail.com>
2023-03-14 14:43:44 +0100

    small reorganization

--- include/ray.h
+++ include/ray.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ray.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amorvai <amorvai@student.42.fr>            +#+  +:+       +#+        */
+/*   By: amorvai <amorvai@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 11:32:04 by amorvai           #+#    #+#             */
-/*   Updated: 2023/03/13 16:03:34 by amorvai          ###   ########.fr       */
+/*   Updated: 2023/03/14 14:34:37 by amorvai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,17 @@
 # include "vector.h"
 # include "scene.h"
 
+# include <stdlib.h> // uint_t
+
 typedef struct s_ray
 {
 	t_point3	orig;
 	t_vec3		dir;
 }				t_ray;
 
+uint32_t	pixel_color(t_scene *scene, int x, int y);
+
 t_ray		ray_constr(const t_point3 origin, const t_vec3 direction);
-
 t_point3	ray_at(const t_ray ray, const double t);
-
-t_ray		get_next_ray(int x, int y, t_image img, t_camera cam);
-t_color		ray_color(const t_ray r, t_scene *scene);
 
 #endif
--- src/graphics/display.c
+++ src/graphics/display.c
@@ -3,20 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   display.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amorvai <amorvai@student.42.fr>            +#+  +:+       +#+        */
+/*   By: amorvai <amorvai@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 19:40:31 by jmaalouf          #+#    #+#             */
-/*   Updated: 2023/03/13 15:31:46 by amorvai          ###   ########.fr       */
+/*   Updated: 2023/03/14 14:33:43 by amorvai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MLX42.h"
 #include "errors.h"
-#include "debug.h"
-#include "color.h"
 #include "scene.h"
 #include "ray.h"
-#include "hittable.h"
 
 #include <stdio.h> // printf
 
@@ -29,24 +26,11 @@ void	key_hook(mlx_key_data_t keydata, void *param)
 		mlx_close_window(mlx);
 }
 
-void		pixel_color(const t_ray r, t_scene *scene, t_color *color, int i)
-{
-	*color = vec3_add(*color, ray_color(r, scene));
-	if (i == SPP - 1)
-	{
-		// printf("hi %i\n", i);
-		*color = vec3_scale_div(*color, SPP);
-	}
-}
-
 void	scene_render(t_scene *scene, mlx_image_t *mlx_img)
 {
 	int			x;
 	int			y;
-	t_ray		r;
-	int			i;
-	t_color		color;
-
+	
 	y = 0;
 	while (y < scene->image.height)
 	{
@@ -54,15 +38,7 @@ void	scene_render(t_scene *scene, mlx_image_t *mlx_img)
 		x = 0;
 		while (x < scene->image.width)
 		{
-			i = 0;
-			while (i < SPP || i == 0)
-			{
-				r = get_next_ray(scene->image.width - x, scene->image.height - y,
-						scene->image, scene->camera);
-				pixel_color(r, scene, &color, i);
-				i++;
-			}
-			mlx_put_pixel(mlx_img, x, y, translate_colors(color.e[0], color.e[1], color.e[2]));
+			mlx_put_pixel(mlx_img, x, y, pixel_color(scene, x, y));
 			x++;
 		}
 		y++;
