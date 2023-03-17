fc94cc8ba186cb1e7dc0aa58a1015c1669b579e6
Jad Maalouf <jad2maalouf@gmail.com>
2023-03-17 09:08:44 +0100

    changes made as requested by her majesty

--- include/ray.h
+++ include/ray.h
@@ -3,14 +3,12 @@
 /*                                                        :::      ::::::::   */
 /*   ray.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmaalouf <jmaalouf@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: amorvai <amorvai@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 11:32:04 by amorvai           #+#    #+#             */
-/*   Updated: 2023/03/15 09:27:10 by jmaalouf         ###   ########.fr       */
+/*   Updated: 2023/03/14 14:34:37 by amorvai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-
 
 #ifndef RAY_H
 # define RAY_H
@@ -19,7 +17,6 @@
 # include "scene.h"
 
 # include <stdlib.h> // uint_t
-# include "scene.h"
 
 typedef struct s_ray
 {
